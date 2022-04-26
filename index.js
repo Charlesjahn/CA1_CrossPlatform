15cf65470d5fdb968564337837f1c329bf20a944
Bekardi <Abdykarimovbekezhan@gmail.com>
2022-04-26 16:40:28 +0100

    login form ( updated )
    FIxed the login form, now it doesn't cause an error
    Updated index.js file

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const uuid = require("uuid");
 const { app, BrowserWindow, Menu, ipcMain } = electron;
 const path = require('path');
 const url = require('url');
+const { userInfo } = require("os");
+const { main } = require("json");
 
 let todayWindow;
-//let createWindow;
+let createWindow;
 let listWindow;
 let allServices = [];
 let allUsers = [];
@@ -16,41 +18,47 @@ let allUsers = [];
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-// fs.readFile("db.json", (err, jsonServices) => {
-//   if (!err) {
-//       const oldServices = JSON.parse(jsonServices);
-//       allServices = oldServices;
-//   };
-// });
+fs.readFile("db.json", (err, jsonServices) => {
+    if (!err) {
+        const oldServices = JSON.parse(jsonServices);
+        allServices = oldServices;
+    };
+});
 
 // Create a new BrowserWindow when `app` is ready
 function createMainWindow() {
 
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    title: "CA Bikes service booking",
-    webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
-      contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
-  // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('login.html')
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        title: "CA Bikes service booking",
+        webPreferences: {
+            // --- !! IMPORTANT !! ---
+            // Disable 'contextIsolation' to allow 'nodeIntegration'
+            // 'contextIsolation' defaults to "true" as from Electron v12
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    })
+    // Load index.html into the new BrowserWindow
+    mainWindow.loadURL(`file://${__dirname}/login.html`);
+    mainWindow.once('ready-to-show', function () {
+        mainWindow.show();
+    });
 
-  // Open DevTools - Remove for PRODUCTION! "comment it"
-  mainWindow.webContents.openDevTools();
+    // Open DevTools - Remove for PRODUCTION! "comment it"
+    mainWindow.webContents.openDevTools();
 
-  // Listen for window being closed
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    // Listen for window being closed
+    mainWindow.on('closed', () => {
+        const jsonAllUsers = JSON.stringify(allUsers);
+        fs.writeFile("users_db.json", jsonAllUsers);
+        app.quit();
+        mainWindow = null;
+    });
 
-  const mainMenu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(mainMenu);
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 };
 
 // Electron `app` is ready
@@ -58,151 +66,156 @@ app.on('ready', createMainWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') app.quit()
+});
 
-// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createMainWindow()
-})
+fs.readFile("users_db.json", (err, jsonAllUsers) => {
+    if (!err) {
+        const oldUsers = JSON.parse(jsonAllUsers);
+        allUsers = oldUsers;
+    }
+});
+
 // when login was successful, it loads today.html window
-ipcMain.on('login', () => {
-  console.log("hiii");
-  mainWindow.loadURL(`file://${__dirname}./today.html`);
-//   mainWindow.on("closed", () => {
-//     const jsonServices = JSON.stringify(allServices);
-//     fs.writeFileSync("db.json", jsonServices);
-//     app.quit();
-//     mainWindow = null;
-// });
+ipcMain.on('login', (event, userName, userPass) => {
+
+    let username = userName;
+    let password = userPass;
+    function Authorization() {
+
+        let user = allUsers[0];
+        console.log(allUsers[0]);
+        if (username === user["username"]) {
+            console.log("working");
+            if (password === user['password']) {
+                console.log("u successfully logged in : " + user["username"]);
+                // mainWindow = todayWindow;
+                mainWindow.loadURL(`file://${__dirname}./today.html`);
+            }
+            else {
+                console.log("wrong password");
+            }
+        } else {
+            console.log(user["username"]);
+            console.log("not working! wrong username!")
+        }
+    }
+    console.log("this is the user that logged in: " + userName + " password " + userPass);
+    Authorization();
 });
 
 
 // creating new booking window
 const createWindowCreator = () => {
-  createWindow = new BrowserWindow({
-      webPreferences: {
-          nodeIntegration: true
-      },
-      min_width: 1000,
-      min_height: 1000,
-      title: "Booking New Service "
-  }); createWindow.setMenu(null);
-  createWindow.loadURL(`file://${__dirname}/create.html`);
-  createWindow.on("closed", () => (createWindow = null));
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        min_width: 1000,
+        min_height: 1000,
+        title: "Booking New Service "
+    }); createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
 };
 // creating new user account @Bekezhan
 const createUserCreator = () => {
-  createWindow = new BrowserWindow({
-      webPreferences: {
-          nodeIntegration: true
-      },
-      min_width: 1000,
-      min_height: 1000,
-      title: "Creating New User"
-  }); 
-  createWindow.setMenu(null);
-  createWindow.loadURL(`file://${__dirname}/user_registration.html`);
-  createWindow.on("closed", () => (createWindow = null));
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        min_width: 1000,
+        min_height: 1000,
+        title: "Creating New User"
+    });
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/user_registration.html`);
+    createWindow.on("closed", () => (createWindow = null));
 };
 
 ipcMain.on("user:createNewUser", (event, newUserAccountForm) => {
-  newUserAccountForm["id"] = uuid();
-  newUserAccountForm["done"] = 0;
-  allUsers.push(newUserAccountForm);
-  createWindow.close();
+    newUserAccountForm["id"] = uuid();
+    newUserAccountForm["done"] = 0;
+    allUsers.push(newUserAccountForm);
+    createWindow.close();
 });
-
-ipcMain.on("user:request:listOfAllUsers", event => {
-  listWindow.webContents.send("user:response:listOfAllUsers", allUsers);
-});
-
-fs.readFile("./users_db.json", (err, jsonAllUsers) => {
-  if (!err) {
-      const oldUsers = JSON.parse(jsonAllUsers);
-      jsonAllUsers = oldUsers;
-  }
-});
-const jsonAllUsers = JSON.stringify(allUsers);
-fs.writeFileSync("./users_db.json", jsonAllUsers);
-
 
 
 // creating all Services window
 
 const listWindowCreator = () => {
-  listWindow = new BrowserWindow({
-      webPreferences: {
-          nodeIntegration: true
-      },
-      min_width: 700,
-      min_height: 700,
-      title: "All Services"
-  });
-  listWindow.setMenu(null);
-  listWindow.loadURL(`file://${__dirname}/list.html`);
-  listWindow.on("closed", () => (listWindow = null));
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        min_width: 700,
+        min_height: 700,
+        title: "All Services"
+    });
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
 };
 ipcMain.on("servicesBikes:create", (event, servicesBikes) => {
-  servicesBikes["id"] = uuid();
-  servicesBikes["done"] = 0;
-  allServices.push(servicesBikes);
-  sendTodayServices();
-  createWindow.close();
+    servicesBikes["id"] = uuid();
+    servicesBikes["done"] = 0;
+    allServices.push(servicesBikes);
+    sendTodayServices();
+    createWindow.close();
 });
 ipcMain.on("servicesBikes:request:list", event => {
-  listWindow.webContents.send("servicesBikes:response:list",
-      allServices);
+    listWindow.webContents.send("servicesBikes:response:list",
+        allServices);
 });
 ipcMain.on("servicesBikes:request:today", event => {
-  sendTodayServices();
+    sendTodayServices();
 });
 ipcMain.on("servicesBikes:done", (event, id) => {
-  allServices.forEach(servicesBikes => {
-      if (servicesBikes.id === id) servicesBikes.done = 1;
-  });
-  sendTodayServices();
+    allServices.forEach(servicesBikes => {
+        if (servicesBikes.id === id) servicesBikes.done = 1;
+    });
+    sendTodayServices();
 });
 const sendTodayServices = () => {
-  const today = new Date().toISOString().slice(0, 10);
-  const filtered = allServices.filter(
-      servicesBikes => servicesBikes.dateSer === today
-  );
-  todayWindow.webContents.send("servicesBikes:response:today", filtered);
+    const today = new Date().toISOString().slice(0, 10);
+    const filtered = allServices.filter(
+        servicesBikes => servicesBikes.dateSer === today
+    );
+    mainWindow.webContents.send("servicesBikes:response:today", filtered);
 };
 const menuTemplate = [
-  {
-      label: "File",
-      submenu: [
-          {
-              label: "New Services",
-              click() {
-                  createWindowCreator();
-              }
-          },
-          {
-              label: "All Services",
-              click() {
-                  listWindowCreator();
-              }
-          },
-          {
-              label: "Create User",
-              click() {
-                  createUserCreator();
-              }
-          },
-          {
-              label: "Quit",
-              accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
-              click() {
-                  app.quit();
-              }
-          }
-      ]
-  },
-  {
-      label: "View",
-      submenu: [{ role: "reload" }, { role: "toggledevtools" }]
-  }
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "New Services",
+                click() {
+                    createWindowCreator();
+                }
+            },
+            {
+                label: "All Services",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Create User",
+                click() {
+                    createUserCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: "View",
+        submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+    }
 ];
--- login.js
+++ login.js
@@ -3,78 +3,102 @@ const fs = require("fs");
 const electron = require("electron");
 const { ipcRenderer } = electron;
 const form = document.querySelector("form"),
-eField = form.querySelector(".email"), 
-eInput = eField.querySelector("input"), //username input
-pField = form.querySelector(".password"),
-pInput = pField.querySelector("input"); //password input
+  eField = form.querySelector(".email"),
+  eInput = eField.querySelector("input"), //username input
+  pField = form.querySelector(".password"),
+  pInput = pField.querySelector("input"); //password input
 
 let allUsers = [];
 
-fs.readFile("users_db.json", (err, jsonAllUsers) => {
-    if (!err) {
-        const oldUsers = JSON.parse(jsonAllUsers);
-        allUsers = oldUsers;
-    }
-});
+// fs.readFile("users_db.json", (err, jsonAllUsers) => {
+//   if (!err) {
+//     const oldUsers = JSON.parse(jsonAllUsers);
+//     allUsers = oldUsers;
+//   }
+// });
 
-function Authorization(){
-    
-   let user = allUsers[0];
-    // console.log(eInput.value);
-    // if(eInput.value === user["username"]){
-    //     console.log("working");
-    //     if(pInput.value === user['password']){
-    //         console.log("correct")
+form.onsubmit = (event) => {
+  event.preventDefault();
+  let userName = eInput.value;
+  let userPass = pInput.value;
+  ipcRenderer.send('login', userName, userPass);
 
-    //             ipcRenderer.send("login", user);
-    //     }
-    //     else{
-    //         console.log("wrong password");
-    //     }
-    // }else{
-    //     console.log(user["username"]);
-    //     console.log("not working! wrong username!")
-    // }
-    ipcRenderer.send("login", user);
+
 }
 
-form.onsubmit = (e)=>{
-  e.preventDefault(); //preventing from form submitting
-  //if email and password is blank then add shake class in it else call specified function
-  (eInput.value == "") ? eField.classList.add("shake", "error") : checkUsername();
-  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
-  setTimeout(()=>{ //remove shake class after 500ms
-    eField.classList.remove("shake");
-    pField.classList.remove("shake");
-  }, 500);
-  eInput.onkeyup = ()=>{checkUsername();} //calling checkEmail function on email input keyup
-  pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
-  
-  function checkUsername(){ //checkUsername function
-    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate username
-    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
-      eField.classList.add("error");
-      eField.classList.remove("valid");
-      let errorTxt = eField.querySelector(".error-txt");
-      //if email value is not empty then show please enter valid email else show Email can't be blank
-      (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
-    }else{ //if pattern matched then remove error and add valid class
-      eField.classList.remove("error");
-      eField.classList.add("valid");
-    }
-  }
-  function checkPass(){ //checkPass function
-    if(pInput.value == ""){ //if pass is empty then add error and remove valid class
-      pField.classList.add("error");
-      pField.classList.remove("valid");
-    }else{ //if pass is empty then remove error and add valid class
-      pField.classList.remove("error");
-      pField.classList.add("valid");
-    }
-  }
-  //if eField and pField doesn't contains error class that mean user filled details properly
-  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
-   // window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
-    Authorization();
+function Authorization() {
+
+  // fs.readFile("users_db.json", (err, jsonAllUsers) => {
+  //   if (!err) {
+  //     const oldUsers = JSON.parse(jsonAllUsers);
+  //     allUsers = oldUsers;
+  //   }
+  // });
+
+  let user = allUsers[0];
+  console.log(allUsers[0]);
+  // console.log(eInput.value);
+  // if(eInput.value === user["username"]){
+  //     console.log("working");
+  //     if(pInput.value === user['password']){
+  //         console.log("correct")
+
+  //             ipcRenderer.send("login", user);
+  //     }
+  //     else{
+  //         console.log("wrong password");
+  //     }
+  // }else{
+  //     console.log(user["username"]);
+  //     console.log("not working! wrong username!")
+  // }
+  // console.log("blyat");
+  // const jsonAllUsers = JSON.stringify(allUsers);
+  // console.log(jsonAllUsers);
+  // console.log(allUsers);
+  // fs.writeFile("users_db.json", jsonAllUsers);
+  // ipcRenderer.send("login", user);
 }
-}
+
+
+
+// form.onsubmit = (e) => {
+//   e.preventDefault(); //preventing from form submitting
+//   //if email and password is blank then add shake class in it else call specified function
+//   (eInput.value == "") ? eField.classList.add("shake", "error") : checkUsername();
+//   (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+//   setTimeout(() => { //remove shake class after 500ms
+//     eField.classList.remove("shake");
+//     pField.classList.remove("shake");
+//   }, 500);
+//   // eInput.onkeyup = ()=>{checkUsername();} //calling checkEmail function on email input keyup
+//   //pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+
+//   function checkUsername() { //checkUsername function
+//     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate username
+//     if (!eInput.value.match(pattern)) { //if pattern not matched then add error and remove valid class
+//       eField.classList.add("error");
+//       eField.classList.remove("valid");
+//       let errorTxt = eField.querySelector(".error-txt");
+//       //if email value is not empty then show please enter valid email else show Email can't be blank
+//       (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+//     } else { //if pattern matched then remove error and add valid class
+//       eField.classList.remove("error");
+//       eField.classList.add("valid");
+//     }
+//   }
+//   function checkPass() { //checkPass function
+//     if (pInput.value == "") { //if pass is empty then add error and remove valid class
+//       pField.classList.add("error");
+//       pField.classList.remove("valid");
+//     } else { //if pass is empty then remove error and add valid class
+//       pField.classList.remove("error");
+//       pField.classList.add("valid");
+//     }
+//   }
+//   //if eField and pField doesn't contains error class that mean user filled details properly
+//   if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+//     // window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
+//     Authorization();
+//   }
+// }
